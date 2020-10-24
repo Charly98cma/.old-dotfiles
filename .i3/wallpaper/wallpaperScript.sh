88b906246e8469f1253a8fa2fccdbbf0dc8f29d5
Charly98cma <1998cma@gmail.com>
2020-10-24 23:31:37 +0200

    Changed wallpaper script and file names

--- .i3/wallpaper/wallpaperScript.sh
+++ .i3/wallpaper/wallpaperScript.sh
@@ -1,44 +1,4 @@
 #!/bin/bash
 
-CURRENTMONTH=$(eval "date +%m")
-
-case $CURRENTMONTH in
-	"01")
-		wallpaper="Enero"	
-	;;
-	"02")
-		wallpaper="Febrero"
-	;;
-	"03")
-		wallpaper="Marzo"
-	;;
-	"04")
-		wallpaper="Abril"
-	;;
-	"05")
-		wallpaper="Mayo"
-	;;
-	"06")
-		wallpaper="Junio"
-	;;
-	"07")
-		wallpaper="Julio"
-	;;
-	"08")
-		wallpaper="Agosto"
-	;;
-	"09")
-		wallpaper="Septiembre"
-	;;
-	"10")
-		wallpaper="Octubre"
-	;;
-	"11")
-		wallpaper="Noviembre"
-	;;
-	"12")
-		wallpaper="Diciembre"
-	;;
-esac
-
-eval "nitrogen --set-centered ~/.dotfiles/.i3/wallpaper/ESA-Calendar/Sentinel-Calendar-'$wallpaper'.jpg"
+CURRENTMONTH=$(eval "date +%m");
+eval "nitrogen --set-centered ~/.dotfiles/.i3/wallpaper/ESA-Calendar/Sentinel-Calendar-$CURRENTMONTH.jpg";
