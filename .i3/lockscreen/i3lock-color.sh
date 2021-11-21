149763a17febbfbaf94eecacedec2e075ce42edf
Charly98cma <1998cma@gmail.com>
2021-11-21 17:16:32 +0100

    Minor changes (moving stuff around)

--- .i3/lockscreen/i3lock-color.sh
+++ .i3/lockscreen/i3lock-color.sh
@@ -8,7 +8,11 @@ indicatorPos=1066:434
 
 dateSize=20
 timeSize=25
+batSize=50
 
+greetFont="terminus"
+
+batLvl=$(eval cat /sys/class/power_supply/BAT0/capacity)"%"
 
 i3lock \
     --image=${wallpaperPath}\
@@ -27,14 +31,19 @@ i3lock \
     --lockfailed-text="Failed to lock... Try again" \
     --noinput-text="" \
     --verif-color=ACAEE8FF \
+    --verif-pos=${textPos} \
+    --verif-text="Checking Starfleet identity..." \
     --wrong-color=FF0000FF \
     --wrong-pos=${textPos} \
-    --verif-pos=${textPos} \
+    --wrong-text="Logging failed... RED ALERT!" \
     --ind-pos=${indicatorPos} \
     --radius=146 \
     --ring-color=${color} \
     --inside-color=00000000 \
-    --verif-text="Checking Starfleet identity..." \
-    --wrong-text="Logging failed... RED ALERT!" \
     --lock-text="" \
+    --greeter-color=${color} \
+    --greeter-size=${batSize} \
+    --greeter-pos="140:820" \
+    --greeter-font=${greetFont} \
+    --greeter-text="${batLvl}"
     
